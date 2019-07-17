2405f0d08097c81b2ae3986485acb5bad6ac78eb
nikkorenz <nikko.clarin@gmail.com>
2019-07-17 22:04:28 +1000

    react-proj

---
+++ precache-manifest.f1631ca08f38dfe8720712021e818666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abdbc8482c08b7fcce20e269e92c570f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "556c721bf86562035780",
+    "url": "/static/css/main.5d69f97c.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "556c721bf86562035780",
+    "url": "/static/js/main.af8b731d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
